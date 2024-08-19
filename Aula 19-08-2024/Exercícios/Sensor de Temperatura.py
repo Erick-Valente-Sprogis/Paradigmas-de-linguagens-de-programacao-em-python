5e6adbff14707ee94de74d3070fa20372353a8b1
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-08-19 20:03:11 -0300

    Update Sensor de Temperatura.py

--- Aula 19-08-2024/Exercícios/Sensor de Temperatura.py
+++ Aula 19-08-2024/Exercícios/Sensor de Temperatura.py
@@ -2,20 +2,18 @@
 
 num1 = float(input("Primeiro Sensor: "))
 num2 = float(input("Segundo Sensor: "))
+media = (num1 + num2) / 2
 
-if(num1+num2)/2 >= 30:
+if (num1 + num2) / 2 >= 30:
   print("Está quente")
-  print((num1+num2)/2)
-  
-elif(num1+num2)/2 >= 20:
+
+elif (num1 + num2) / 2 >= 20:
   print("Um pouco quente")
-  print((num1+num2)/2)
-  
-elif(num1+num2)/2 >= 10:
+
+elif (num1 + num2) / 2 >= 10:
   print("Um pouco frio")
-  print((num1+num2)/2)
   
 else:
   print("Está frio")
-  print((num1+num2)/2)
+print(media)
 print("END")
