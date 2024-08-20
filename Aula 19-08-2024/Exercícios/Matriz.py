f96cee65e17baaa235e809bc9a9cfae6c6c7deda
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-08-19 21:28:51 -0300

    Update Matriz.py

--- Aula 19-08-2024/Exercícios/Matriz.py
+++ Aula 19-08-2024/Exercícios/Matriz.py
@@ -1,7 +1,5 @@
-def main():
-  a = [0, 1, 2, 3, 4]
-  b = a[:]
-  b[1] = 7
-  print("a=",a)
-  print("b=",b)
-main()
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+  for c in range(0,3):
+    matriz[l][c] = int(input('Digite um valor para [{l}, {c}]:'))
+print(matriz)
