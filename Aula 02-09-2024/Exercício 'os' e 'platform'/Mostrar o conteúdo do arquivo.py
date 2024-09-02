76936c57e3319a1958552cb35ddb888d7cf8e321
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-09-02 20:29:23 -0300

    Delete Aula 02-09-2024/Exercício 'os' e 'platform'/Mostrar o conteúdo do arquivo.py

--- Aula 02-09-2024/Exercício 'os' e 'platform'/Mostrar o conteúdo do arquivo.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-
-def mostrar_conteudo_arquivo(nome_arquivo):
-    if verificar_existe_arquivo(nome_arquivo) and verificar_arquivo_nao_vazio(nome_arquivo):
-        with open(nome_arquivo, 'r') as arquivo:
-            return arquivo.read()
-    else:
-        return "O arquivo não existe ou está vazio."
