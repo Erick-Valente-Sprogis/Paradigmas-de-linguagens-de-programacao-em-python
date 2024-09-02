b64e749ba12d47c37514959a5b5ef2f8a790a4c8
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-09-02 20:29:16 -0300

    Delete Aula 02-09-2024/Exercício 'os' e 'platform'/Contar a quantidade de linhas do arquivo.py

--- Aula 02-09-2024/Exercício 'os' e 'platform'/Contar a quantidade de linhas do arquivo.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-
-def contar_linhas_arquivo(nome_arquivo):
-    if verificar_existe_arquivo(nome_arquivo) and verificar_arquivo_nao_vazio(nome_arquivo):
-        with open(nome_arquivo, 'r') as arquivo:
-            return sum(1 for _ in arquivo)
-    else:
-        return "O arquivo não existe ou está vazio."
